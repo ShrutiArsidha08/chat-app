0305b9efb54425ad85fa254efcd741d91fe2a202
rarsidha <rarsidha@gmail.com>
2024-06-07 18:30:38 +0530

    .

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import 'resuite/dist/styles/resuite-default.css';
+import './styles/main.scss';
+
+function App() {
+  return (
+    <div>
+hello
+    </div>
+  );
+}
+
+export default App;
